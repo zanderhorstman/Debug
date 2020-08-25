af21902c2184b6948be2423466e270eefe4027f7
zanderhorstman <zanderhorstman@gmail.com>
2020-08-25 10:16:53 -0400

    2

--- src/Debug.java
+++ src/Debug.java
@@ -2,16 +2,14 @@ import java.util.Scanner;
 
 public class Debug {
     public static void main(String[] args) {
-        String name;
-        int age;
-        int dob;
+
         Scanner keybd = new Scanner(System.in);
         System.out.print("Input your name: ");
-        name = keybd.nextLine();
+        String name = keybd.nextLine();
         System.out.print("Please enter your age: ");
-        age = keybd.nextInt();
+        int age = keybd.nextInt();
         System.out.print("Please enter your birth year: ");
-        dob= keybd.nextInt();
+        int dob= keybd.nextInt();
         System.out.println("Summary:\nYour name is: " + name);
         System.out.println("\nYour age is " + age);
         System.out.println("\nYour birth year is: " +dob);
